f1ce37e7ef742ea5784b64421b7c6213021e1d10
emuller1996 <stefano.muller@hotmail.com>
2023-11-06 17:45:32 -0500

    fix no se

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,5 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+/* import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default prisma;
+ */
+
+import { PrismaClient } from "@prisma/client"
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+const globalForPrisma = globalThis
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
