c89ddff8180c326d7eaac3326481297512101bb3
emuller1996 <stefano.muller@hotmail.com>
2023-11-17 22:18:08 -0500

    remove

--- src/app/pagos/page.jsx
+++ src/app/pagos/page.jsx
@@ -3,7 +3,7 @@ import TablePagos from "./components/TablePagos";
 export default async function PagosPage(){
     return(
         <div className="container mx-auto">
-            <div className="text-center mb-5">Todo los Pagos</div>
+            <div className="text-center mb-5">Todo los Pago</div>
             <hr />
             <TablePagos />
         </div>
